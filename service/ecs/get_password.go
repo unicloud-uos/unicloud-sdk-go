e6d5a63476f1089bcca55d7831637a6b615f1d23
LC <943222539@qq.com>
2021-04-08 16:48:29 +0800

    实现查看云主机初始密码接口

---
+++ service/ecs/get_password.go
@@ -0,0 +1,43 @@
+package ebs
+
+import (
+	"encoding/json"
+	tchttp "github.com/unicloud-uos/unicloud-sdk-go/sdk/common/http"
+)
+
+type GetPasswordRequest struct {
+	*tchttp.BaseRequest
+	InstanceId string `name:"InstanceId"`
+}
+
+type GetPasswordResponse struct {
+	*tchttp.BaseResponse
+	RequestId string `name:"RequestId" string`
+	Password  string `name:"password" string`
+}
+
+func (r *GetPasswordResponse) ToJsonString() string {
+	b, _ := json.Marshal(r)
+	return string(b)
+}
+
+func NewGetPasswordRequest() (request *GetPasswordRequest) {
+	request = &GetPasswordRequest{
+		BaseRequest: &tchttp.BaseRequest{},
+	}
+	request.Init().WithApiInfo(Service, APIVersion, "GetEcsPassword")
+	return
+}
+
+func NewGetPasswordResponse() (response *GetPasswordResponse) {
+	response = &GetPasswordResponse{
+		BaseResponse: &tchttp.BaseResponse{},
+	}
+	return
+}
+
+func (c *Client) GetPassword(request *GetPasswordRequest) (response *GetPasswordResponse, err error) {
+	response = NewGetPasswordResponse()
+	err = c.Send(request, response)
+	return
+}
